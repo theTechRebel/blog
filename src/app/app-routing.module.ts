7caf8fbb95060888040a253941b3f643d8590202
Tinashe Dumbura <tinashe.dumbura@dariel.co.za>
2020-04-12 02:19:39 +0200

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './compnents/home/home.component';
+import { AuthorComponent } from './compnents/author/author.component';
+import { PostComponent } from './compnents/post/post.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'author',
+    component: AuthorComponent
+  },
+  {
+    path: 'post',
+    component: PostComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturedComponent } from './compnents/featured/featured.component';
+import { HomeComponent } from './compnents/home/home.component';
+import { FooterComponent } from './compnents/footer/footer.component';
+import { RecentComponent } from './compnents/recent/recent.component';
+import { AuthorComponent } from './compnents/author/author.component';
+import { PostComponent } from './compnents/post/post.component';
+import { RelatedComponent } from './compnents/related/related.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturedComponent,
+    HomeComponent,
+    FooterComponent,
+    RecentComponent,
+    AuthorComponent,
+    PostComponent,
+    RelatedComponent
   ],
   imports: [
     BrowserModule,
