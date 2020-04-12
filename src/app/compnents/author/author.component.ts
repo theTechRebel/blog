aa2b9698435a1b7fa199837a17404c4e5cc7c22d
Tinashe Dumbura <tinashe.dumbura@dariel.co.za>
2020-04-12 03:13:14 +0200

    fixed twitter widget

--- src/app/compnents/author/author.component.ts
+++ src/app/compnents/author/author.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.css']
 })
-export class AuthorComponent implements OnInit {
+export class AuthorComponent implements OnInit, AfterViewInit  {
 
   constructor() { }
+  ngAfterViewInit(): void {
+     // @ts-ignore
+     twttr.widgets.load();
+  }
 
   ngOnInit(): void {
   }
